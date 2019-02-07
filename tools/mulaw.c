b39001531dd95f684e2c60669a910c04f1a95e87
superctr <themvrules@gmail.com>
2019-02-07 07:23:42 +0100

    update mulaw.c

--- tools/mulaw.c
+++ tools/mulaw.c
@@ -7,83 +7,84 @@
 int main(int argc, char* argv [])
 {
 
-    unsigned long i;
+	unsigned long i;
 
-    if(argc<3)
-    {
-        printf("Usage: <file> <destination file> \n");
-        exit(EXIT_FAILURE);
-    }
-
-    char* file1 = argv[1];
-    char* file2 = argv[2];
-/*
-    int16_t mulaw_tab[256];
-
-    double x_max = 32752.0;
-    double y_max = 127.0;
-    double u = 10.0;
-
-    for(i=0;i<256;i++)
-    {
-        double y = (double) (i & 0x7f);
-        double x = (exp (y / y_max * log (1.0 + u)) - 1.0) * x_max / u;
-        if (i & 0x80)
-            x = -x;
-        mulaw_tab[i] = (int16_t)x;
+	printf("C352 Mulaw->LPCM conversion tool\n");
+	printf("Output format: signed 16 bit PCM little endian\n");
+		
+	if(argc<3)
+	{
+		printf("Usage: <file> <destination file> \n");
+		exit(EXIT_FAILURE);
 	}
-*/
-    int res;
 
-    FILE* sourcefile;
-    /* Load sample file */
-    sourcefile = fopen(file1,"rb");
-    if(!sourcefile)
-    {
-        printf("Could not open %s\n",file1);
-        exit(EXIT_FAILURE);
-    }
-    fseek(sourcefile,0,SEEK_END);
-    unsigned long sourcefile_size = ftell(sourcefile);
-    rewind(sourcefile);
-    uint8_t* source = (uint8_t*)malloc(sourcefile_size);
-    uint16_t* dest = (uint16_t*)malloc(sourcefile_size*2);
-    res = fread(source,1,sourcefile_size,sourcefile);
-    if(res != sourcefile_size)
-    {
-        printf("Reading error\n");
-        exit(EXIT_FAILURE);
-    }
-    fclose(sourcefile);
+	char* file1 = argv[1];
+	char* file2 = argv[2];
 
-    int length = sourcefile_size;
-	int8_t s, s2;
+	int16_t mulaw_tab[256];
 
-    for(i=0;i<length;i++)
-    {
-		s = source[i];
+	int j=0;
 
-		s2 = s&0x7f;
-		dest[i] = (s2*s2)<<1;
-		if(s&0x80)
-			dest[i] = -dest[i];
-        //dest[i] = mulaw_tab[source[i]];
-    }
-    printf("memory ok %lu\n",length);
+	for(i=0;i<128;i++)
+	{
+		mulaw_tab[i] = j<<5;
+		if(i < 16)
+			j += 1;
+		else if(i < 24)
+			j += 2;
+		else if(i < 48)
+			j += 4;
+		else if(i < 100)
+			j += 8;
+		else
+			j += 16;
+	}
+	for(i=128;i<256;i++)
+		mulaw_tab[i] = (~mulaw_tab[i-128])&0xffe0;
+	int res;
 
-    FILE *destfile;
-    destfile = fopen(file2,"wb");
-    if(!destfile)
-    {
-        printf("Could not open %s\n",file2);
-        exit(EXIT_FAILURE);
-    }
-    for(i=0;i<sourcefile_size*2;i++)
-    {
-        putc(*((uint8_t*)dest+i),destfile);
-    }
-    fclose(destfile);
-    printf("write ok %lu\n",length);
+	FILE* sourcefile;
+	/* Load sample file */
+	sourcefile = fopen(file1,"rb");
+	if(!sourcefile)
+	{
+		printf("Could not open %s\n",file1);
+		exit(EXIT_FAILURE);
+	}
+	fseek(sourcefile,0,SEEK_END);
+	unsigned long sourcefile_size = ftell(sourcefile);
+	rewind(sourcefile);
+	uint8_t* source = (uint8_t*)malloc(sourcefile_size);
+	uint16_t* dest = (uint16_t*)malloc(sourcefile_size*2);
+	res = fread(source,1,sourcefile_size,sourcefile);
+	if(res != sourcefile_size)
+	{
+		printf("Reading error\n");
+		exit(EXIT_FAILURE);
+	}
+	fclose(sourcefile);
 
-    free(source);
+	int length = sourcefile_size;
+
+	for(i=0;i<length;i++)
+	{
+		dest[i] = mulaw_tab[source[i]];
+	}
+	printf("memory ok %lu\n",length);
+
+	FILE *destfile;
+	destfile = fopen(file2,"wb");
+	if(!destfile)
+	{
+		printf("Could not open %s\n",file2);
+		exit(EXIT_FAILURE);
+	}
+	for(i=0;i<sourcefile_size*2;i++)
+	{
+		putc(*((uint8_t*)dest+i),destfile);
+	}
+	fclose(destfile);
+	printf("write ok %lu\n",length);
+
+	free(source);
 }
